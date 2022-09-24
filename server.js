a8f9626f31f885c7c2be30b61f0ff1093389e1b5
Dan Thomas <104170584+RetroColossus@users.noreply.github.com>
2022-09-24 14:58:46 -0400

    initial

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
@@ -5,12 +6,20 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:127.0.0.1:27017/social-network-api',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`🌍 Connected on localhost:${PORT}`);
   });
 });
